320ae363a0d5379c348e01a041790907361676bc
GABRIEL KOEHLER DA SILVA <gabriel_k_da-silva@estudante.sesisenai.org.br>
2023-04-27 21:53:30 -0300

    todo app realizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+interface Tarefa{
+  nome:string,
+  categoria:string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'todo-app';
+
+  nome:string = ''
+  categoria:string =''
+tarefa: Tarefa={
+ nome: this.nome,
+ categoria: this.categoria
+}
+  tarefas: Tarefa[]=[]
+
+ cadastrarUsuario():void{
+
+    const tarefa: Tarefa={
+      nome: this.tarefa.nome,
+      categoria: this.tarefa.categoria
+    }
+
+    this.tarefas.push(tarefa)
+    console.log(this.tarefas)
+    this.tarefa.nome=''
+ }
+ removerTarefa(indice):void{
+    this.tarefas.slice(indice,indice+1)
+ }
 }
