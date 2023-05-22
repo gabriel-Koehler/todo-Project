d63e334575577a8959acc0cfba642e4b1f0a0c01
GABRIEL KOEHLER DA SILVA <gabriel_k_da-silva@estudante.sesisenai.org.br>
2023-05-22 16:22:10 -0300

    melhora 22/05

--- src/app/AdicionaCategoria/adicionaCategoria.component.ts
+++ src/app/AdicionaCategoria/adicionaCategoria.component.ts
@@ -3,6 +3,11 @@ interface Categoria{
     categoria:string,
     corFundo:string
 }
+interface Tarefa{
+    titulo:string,
+    categoria:string,
+    descricao:string
+  }
 @Component({
     selector:'adiciona-categoria',
     templateUrl:'./adicionaCategoria.component.html',
@@ -12,10 +17,15 @@ export class AdicionaCategoriaComponent{
 categorias:Categoria[]=[]
 nomeCategoria:string =''
 corEscolhida:string=''
+ tarefas:Tarefa[]=[]
     ngOnInit():void{
         if(JSON.parse(localStorage.getItem('categorias'))!=null){
             this.categorias = JSON.parse(localStorage.getItem('categorias'))
         }
+        if(localStorage.getItem('tarefas')!=null){
+            this.tarefas = JSON.parse(localStorage.getItem('tarefas'))
+            console.log(this.tarefas)
+            }
     }
 
     adicionarCategoria():void{         
@@ -30,5 +40,16 @@ corEscolhida:string=''
          this.nomeCategoria=''
          this.corEscolhida='#ffff'
     }
-    
+    removeCategoria(id):void{
+        for(let tarefa of this.tarefas){
+            console.log(this.tarefas)
+            if(tarefa.categoria==this.categorias[id].categoria){
+              this.tarefas.splice(this.tarefas.indexOf(tarefa),1)
+            }
+          }
+          
+  this.categorias.splice(id,1)
+  localStorage.setItem('categorias',JSON.stringify(this.categorias))
+  localStorage.setItem('tarefas',JSON.stringify(this.tarefas))
+    }
 }
